2d1e3d2c82673973226d8f89c92ccc2cf5607903
AmorphousPhage <151792941+amorphousphage@users.noreply.github.com>
2025-03-09 16:36:54 +0000

    Delete bitcoin-tracker-backup.sh

--- bitcoin-tracker-backup.sh
+++
@@ -1,5 +0,0 @@
- #!/bin/bash
-BACKUP_DIR="/mount/nvme/Hosted_Services/backups/Bitcoin_Tracker_backup"
-BACKUP_FILE_NAME="$(date +"%d-%m-%y-%H%M%S.sql")"
-docker exec bitcointracker_db bash -c 'exec mysqldump --databases bitcointracker -u"root" -p"4493abcd"' > "$BACKUP_DIR"/"$BACKUP_FILE_NAME";
-find "$BACKUP_DIR" -type f -name "*.sql" -mtime +15 -delete
